d619a35ff473f308c21a2882b9437111df650879
Kk3tillen <k.kezia@academico.ifrn.edu.br>
2023-07-05 10:19:55 -0300

    first commit

---
+++ aulaPPI/js/dom.js
@@ -0,0 +1,11 @@
+// console.log(document.getElementById("portugues"));
+// console.log(document.querySelector("#portugues"));
+
+// console.log(document.getElementsByTagName("button"));
+// console.log(document.querySelector("button"));
+
+document.querySelector("#ingles").innerHTML = "English";
+console.log(document);
+
+let botao = document.createElement("button");
+botao.innerHTML("Japones");
---
+++ aulaPPI/js/olamundo.js
@@ -0,0 +1,9 @@
+function portugues(){
+    alert("Olá mundo!");
+}
+document.getElementById("portugues").addEventListener("click", portugues);
+
+document.getElementById("alemao").onclick=function(){
+    alert("Hallo Wereld!");
+}
+console.log(document);
